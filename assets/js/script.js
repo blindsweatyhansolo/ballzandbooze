22473aaeaa70baefdec9833b5b04d057e2b17e66
J Semidey <mtsemidey@gmail.com>
2022-03-31 20:58:58 -0400

    function to add game schedule info to container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,62 +3,112 @@
 var inputDate = moment().format("YYYY-MM-DD");
 console.log(inputDate);
 var scheduleContainer = $("#schedule-container");
+var scheduleContent = $("#schedule-content");
+var gameDate = $("#game-date");
 
-var test = $("<p>");
-test.text("boop");
-scheduleContainer.append(test);
+var getSchedules = function() {
+    // nba api api header variables
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", "d94a3deae8mshddff40e63fbd519p190b9cjsna5832c849eab");
+    myHeaders.append("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
+
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    var apiUrl = "https://api-nba-v1.p.rapidapi.com/games?date=" + inputDate;
+
+    //   https://api-nba-v1.p.rapidapi.com/games?date=2022-03-29
+
+    fetch(apiUrl, requestOptions).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        for (var i = 0; i < data.response.length; i++) {
+            
+            
+            var homeTeam = data.response[i].teams.home.name;
+            var homeTeamLogo = data.response[i].teams.home.logo;
+            
+            // console.log(homeTeam);
+            // console.log(homeTeamLogo);
+            
+            var awayTeam = data.response[i].teams.visitors.name;
+            var awayTeamLogo = data.response[i].teams.visitors.logo;
+            // console.log(awayTeam);
+            // console.log(awayTeamLogo);
+
+            
+            var gameStatus = data.response[i].status.long;
+            // console.log(gameStatus);
+            
+            // pass city name as parameter for brewery call
+            var gameCity = data.response[i].arena.city;
+            // console.log(gameCity);
+            
+            // console.log(data);
+
+            var gameCard = $("<div class='columns card is-flex is-justify-content-space-around'>");
+            scheduleContent.append(gameCard);
+
+            var homeCard = $("<div class='card-image mx-3'>");
+            var versusText = $("<div class='mx-3'>");
+            var awayCard = $("<div class='card-image mx-3'>");
+            var gameStatusCard = $("<div class='card'>");
 
 
-// nba api api header variables
-var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "d94a3deae8mshddff40e63fbd519p190b9cjsna5832c849eab");
-myHeaders.append("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
+            var homeTeamName = $("<p class='has-text-weight-bold'>");
+            homeTeamName.text(homeTeam);
+            var homeTeamLogoImg = $("<img class='image is-64x64'>");
+            homeTeamLogoImg.attr("src", "" + homeTeamLogo + "");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+            homeCard.append(homeTeamLogoImg);
+            homeCard.append(homeTeamName);
 
-var apiUrl = "https://api-nba-v1.p.rapidapi.com/games?date=" + inputDate;
+            gameCard.append(homeCard);
 
-//   https://api-nba-v1.p.rapidapi.com/games?date=2022-03-29
+            versusText = $("<p class='has-text-weight-bold'>");
+            versusText.text("VS");
+            gameCard.append(versusText);
 
-fetch(apiUrl, requestOptions).then(function (response) {
-    return response.json();
-}).then(function (data) {
+            var awayTeamName = $("<p class='has-text-weight-bold'>");
+            awayTeamName.text(awayTeam);
+            var awayTeamLogoImg = $("<img class='image is-64x64'>");
+            awayTeamLogoImg.attr("src", "" + awayTeamLogo + "");
 
-    console.log(data);
+            awayCard.append(awayTeamLogoImg);
+            awayCard.append(awayTeamName);
 
-    var homeTeam = data.response[0].teams.home.name;
-    var homeTeamLogo = data.response[0].teams.home.logo;
+            gameCard.append(awayCard);
 
-    console.log(homeTeam);
+            var gameStatusText = $("<p class='has-text-weight-bold'>");
+            gameStatusText.text(gameStatus);
 
-    var awayTeam = data.response[0].teams.visitors.name;
-    var awayTeamLogo = data.response[0].teams.visitors.logo;
-    console.log(awayTeam);
+            gameStatusCard.append(gameStatusText)
 
-    var gameStatus = data.response[0].status.long;
-    console.log(gameStatus);
+            gameCard.append(gameStatusCard);
+        } 
+    }).catch(error => console.log('error', error));;
 
-    // pass city name as parameter for brewery call
-    var gameCity = data.response[0].arena.city;
-    console.log(gameCity);
+}
 
-    // for (var i = 0; i < response.length; i++) {
-    //     console.log(response[i]);
-    // }
+var getBars = function(){
+    // brewery api start
+    // var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_city=san_diego";
+    // var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_postal=21144&by_type=micro";
+    var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_city=springfield";
+    
+    fetch(breweryApiUrl).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.response.length; i++) {
+            console.log(data.response[i].city);
+        }
+    })
+}
 
-}).catch(error => console.log('error', error));;
 
-// brewery api start
-// var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_city=san_diego";
-// var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_postal=21144&by_type=micro";
-var breweryApiUrl = "https://api.openbrewerydb.org/breweries?by_city=springfield"
-
-fetch(breweryApiUrl).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    console.log(data);
-})
+getSchedules();
+getBars();
