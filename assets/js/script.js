14bd79f9d686eeea380166852ca823e520eed031
J Semidey <mtsemidey@gmail.com>
2022-04-05 15:12:24 -0400

    added conditional to handle when no games are scheduled, placeholder for modal set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // var apyKey = "d94a3deae8mshddff40e63fbd519p190b9cjsna5832c849eab"; 
+
 // date using moment.js
 var currentDate = moment().format("YYYY-MM-DD");
 // console.log(inputDate);
@@ -13,7 +14,9 @@ var gameSearchBtn = $("#game-search-btn");
 var datePicker = $("#date-picker");
 var todayGameBtn = $("#today-game");
 
+// function to render game schedules from inputed date
 var getSchedules = function(inputDate) {
+    // parse the stringified date, did this since I was having errors when trying to read implemented inputDate value
     var inputDate = JSON.parse(inputDate);
     console.log(inputDate);
 
@@ -36,74 +39,83 @@ var getSchedules = function(inputDate) {
         return response.json();
     }).then(function (data) {
         console.log(data);
-        for (var i = 0; i < data.response.length; i++) {
-            
-            
-            var homeTeam = data.response[i].teams.home.name;
-            var homeTeamLogo = data.response[i].teams.home.logo;
-            
-            // console.log(homeTeam);
-            // console.log(homeTeamLogo);
-            
-            var awayTeam = data.response[i].teams.visitors.name;
-            var awayTeamLogo = data.response[i].teams.visitors.logo;
-            // console.log(awayTeam);
-            // console.log(awayTeamLogo);
-
-            
-            var gameStatus = data.response[i].status.long;
-            // console.log(gameStatus);
-            
-            // pass city name as parameter for brewery call
-            var gameCity = data.response[i].arena.city;
-            // console.log(gameCity);
-            
-            // console.log(data);
-
-            var gameCard = $("<div class='columns card is-flex is-justify-content-space-around is-v-centered'>");
-            scheduleContent.append(gameCard);
-
-            var homeCard = $("<div class='card-image mx-3'>");
-            var versusText = $("<div class='mx-3'>");
-            var awayCard = $("<div class='card-image mx-3'>");
-            var gameStatusCard = $("<div class='card is-shadowless'>");
-
-
-            var homeTeamName = $("<p class='has-text-weight-bold mx-2'>");
-            homeTeamName.text(homeTeam);
-            var homeTeamLogoImg = $("<img class='image is-48x48 is-inline-block mt-2'>");
-            homeTeamLogoImg.attr("src", "" + homeTeamLogo + "");
-
-            homeCard.append(homeTeamLogoImg);
-            homeCard.append(homeTeamName);
-
-            gameCard.append(homeCard);
-
-            versusText = $("<p class='has-text-weight-bold mt-4 pt-4 my-4'>");
-            versusText.text("VS");
-            gameCard.append(versusText);
-
-            var awayTeamName = $("<p class='has-text-weight-bold mx-2'>");
-            awayTeamName.text(awayTeam);
-            var awayTeamLogoImg = $("<img class='image is-48x48 is-inline-block mt-2'>");
-            awayTeamLogoImg.attr("src", "" + awayTeamLogo + "");
-
-            awayCard.append(awayTeamLogoImg);
-            awayCard.append(awayTeamName);
-
-            gameCard.append(awayCard);
-
-            var gameStatusText = $("<p class='has-text-weight-bold mt-4 pt-4 mr-2'>");
-            gameStatusText.text(gameStatus);
-
-            gameStatusCard.append(gameStatusText)
-
-            gameCard.append(gameStatusCard);
-        } 
-    }).catch(error => console.log('error', error));
+        // if results = 0, there are no games for selected date
+        if (data.results === 0) {
+            console.log("NO GAMES");
+            // make this a modal message!!
+            alert("No Games Scheduled on " + inputDate);
+        } else {
+            // else render game card for each in array
+            for (var i = 0; i < data.response.length; i++) {
+                
+                
+                var homeTeam = data.response[i].teams.home.name;
+                var homeTeamLogo = data.response[i].teams.home.logo;
+                
+                // console.log(homeTeam);
+                // console.log(homeTeamLogo);
+                
+                var awayTeam = data.response[i].teams.visitors.name;
+                var awayTeamLogo = data.response[i].teams.visitors.logo;
+                // console.log(awayTeam);
+                // console.log(awayTeamLogo);
+    
+                
+                var gameStatus = data.response[i].status.long;
+                // console.log(gameStatus);
+                
+                // pass city name as parameter for brewery call
+                var gameCity = data.response[i].arena.city;
+                // console.log(gameCity);
+                
+                // console.log(data);
+    
+                var gameCard = $("<div class='columns card is-flex is-justify-content-space-around is-v-centered'>");
+                scheduleContent.append(gameCard);
+    
+                var homeCard = $("<div class='card-image mx-3'>");
+                var versusText = $("<div class='mx-3'>");
+                var awayCard = $("<div class='card-image mx-3'>");
+                var gameStatusCard = $("<div class='card is-shadowless'>");
+    
+    
+                var homeTeamName = $("<p class='has-text-weight-bold mx-2'>");
+                homeTeamName.text(homeTeam);
+                var homeTeamLogoImg = $("<img class='image is-48x48 is-inline-block mt-2'>");
+                homeTeamLogoImg.attr("src", "" + homeTeamLogo + "");
+    
+                homeCard.append(homeTeamLogoImg);
+                homeCard.append(homeTeamName);
+    
+                gameCard.append(homeCard);
+    
+                versusText = $("<p class='has-text-weight-bold mt-4 pt-4 my-4'>");
+                versusText.text("VS");
+                gameCard.append(versusText);
+    
+                var awayTeamName = $("<p class='has-text-weight-bold mx-2'>");
+                awayTeamName.text(awayTeam);
+                var awayTeamLogoImg = $("<img class='image is-48x48 is-inline-block mt-2'>");
+                awayTeamLogoImg.attr("src", "" + awayTeamLogo + "");
+    
+                awayCard.append(awayTeamLogoImg);
+                awayCard.append(awayTeamName);
+    
+                gameCard.append(awayCard);
+    
+                var gameStatusText = $("<p class='has-text-weight-bold mt-4 pt-4 mr-2'>");
+                gameStatusText.text(gameStatus);
+    
+                gameStatusCard.append(gameStatusText)
+    
+                gameCard.append(gameStatusCard);
+            } 
+        }
+        }).catch(error => console.log('error', error));
 
 }
 
+// function to render bars from city search
 var getBars = function(city){
     // brewery api start
     var cityName = city;
@@ -182,6 +194,7 @@ var getBars = function(city){
 
 }
 
+// function to get value from city search, pass to getBars()
 var formHandler = function(event) {
     event.preventDefault();
 
@@ -196,31 +209,39 @@ var formHandler = function(event) {
     getBars(city);
 };
 
+// function to get value from datepicker, send to getSchedules()
 var gameFormHandler = function(event) {
     // event.preventDefault();
-
+    // clear old content
     scheduleContent.html("");
 
+    // stringify value, was having issues when passing to function, this seemed to work
     var inputDate = JSON.stringify(datePicker.val());
 
     console.log(inputDate);
+    // send value as parameter to getSchedules
     getSchedules(inputDate);
 
 }
 
+// EVENT HANDLERS SECTION //
+
 // event handler for city search input
 citySearch.on("submit", formHandler);
 searchBtn.on("click", formHandler);
+// event handler for datepicker
 gameSearchBtn.on("click", gameFormHandler);
 todayGameBtn.on("click", function(){
+    // use currentDate value set with moment.js
     var inputDate = JSON.stringify(currentDate);
 
+    // clear old content
     scheduleContent.html("");
 
+    // send value as parameter to getSchedules()
     getSchedules(inputDate);
 
-})
-
+});
 
 // getSchedules();
 // getBars();
