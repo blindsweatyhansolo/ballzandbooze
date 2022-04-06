4cef3d1de08f2f275f53b4e748479954736a9ac3
Tay Starr <tstarrroberts@gmail.com>
2022-04-06 14:14:39 -0400

    save favorite bar with refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    renderSavedBars();
+});
+
 // current date using moment.js
 var currentDate = moment().format("YYYY-MM-DD");
 var scheduleContainer = $("#schedule-container");
@@ -229,17 +233,61 @@ var faveBar = function(event) {
         address: barAddress
     };
 
-    var faveBarsArr = localStorage.getItem("faveBarsArr");
+    var faveBarsArr = localStorage.getItem("faveBars");
 
     if(faveBarsArr === null) {
         faveBarsArr = [];
     } else {
         faveBarsArr = JSON.parse(faveBarsArr);
+        renderSavedBars();
+
     }
     faveBarsArr.push(barStorage);
     var newFaveBar = JSON.stringify(faveBarsArr);
     localStorage.setItem("faveBars", newFaveBar);
     console.log(faveBarsArr);
+
+    // renderSavedBars();
+};
+
+var renderSavedBars = function() {
+    var faveBarsArr = localStorage.getItem("faveBars");
+    faveBarsArr = JSON.parse(faveBarsArr);
+
+    for (let i = 0; i < faveBarsArr.length; i++) {
+        var savedBarInfoContainer = $("<div class='is-flex is-flex-direction-row'>");
+        var barCard = $("<div class='card mb-1 px-1 container'>");
+
+        var barCardContent = $("<div class='card-content'>");
+        // barCardContent.attr("id", faveBarsArr[i].name);
+        // barCardContent.text(faveBarsArr[i].name);
+        var barCardFooter = $("<footer class='card-footer'>");
+        var footerUnFavorite = $("<p class='card-footer-item is-clickable'>");
+        // footerFavorite.attr("id", "favorite");
+
+        var footerUnFavoriteSpan = $("<span>");
+        footerUnFavorite.text("Remove Favorite");
+        footerUnFavorite.append(footerUnFavoriteSpan);
+        barCardFooter.append(footerUnFavorite);
+
+
+        var barName = $("<h3 class='barname has-text-weight-semibold has-text-left'>");
+        barName.text(faveBarsArr[i].name);
+
+        var barAddress = $("<address class='baraddress has-text-left'>");
+        // barAddress.attr("id", barStreetAddress);
+        barAddress.text(faveBarsArr[i].address);
+        barCardContent.append(barName);
+        barCardContent.append(barAddress);
+
+        barCard.append(barCardContent);
+        barCard.append(barCardFooter);
+        savedBarInfoContainer.append(barCard);
+        barContainer.append(savedBarInfoContainer);
+
+    console.log(faveBarsArr);
+
+    }
 };
 
 // function to get value from city search, pass to getBars()
